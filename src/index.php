3ae05935691ebde6b52598feb065b85c07c78019
Iverson Correia <51964753+021magrinho@users.noreply.github.com>
2021-10-29 13:53:57 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,3 +1,4 @@
 <html> 
-  <a href="html/login.php">clique aqui2</a>
+  <p>Para se cadastrar, <a href="cadastro.php">clique aqui.</a></p>
+  <p>Para fazer login, <a href="html/login.php">clique aqui.</a></p>
 </html>
